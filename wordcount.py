343698bfe570d3cbe95b3e194a325af7f01e727a
Hackbright Student <user@mini-002.(none)>
2013-09-30 12:44:58 -0700

    first commit

---
+++ wordcount.py
@@ -0,0 +1,62 @@
+
+#lower all characters
+#for loop to run through text
+#split into a list with
+#strip the list items to get rid of non-word characters
+#set default to 1 (dict.setdefault)
+#iterate through list of words (dict.iteritems)
+    #if not in list, set it to default
+    #if in list +=1
+
+
+def main():
+    f = open("twain.txt")
+    filetext = f.read()
+    f.close()
+
+    # lowercase all text
+    filetext = filetext.lower()
+
+    # removes all spaces and creates a list of words
+    words = filetext.split()
+
+    for i in range(len(words)):
+        words[i:i + 1] = words[i].split("--")
+
+    word_count = {}
+
+
+    for i in range(len(words)):
+        # removes periods and commas
+        words[i] = words[i].strip("\"\'.,?():;!_")
+
+        # check if word exists in key-value word_count 
+        word_count.setdefault(words[i], 0)
+        word_count[words[i]] += 1
+
+    # create new dict and list to sort words alphabetically by frequency
+    frequency_of_words = {}
+    frequency_count = []
+
+
+    for key, value in word_count.iteritems():
+        # originally print key, value
+        # sets frequency as key in dict and value is list of 
+        # words that has that frequency
+        frequency_of_words.setdefault(value, [])
+        frequency_of_words[value].append(key)
+        frequency_of_words[value].sort()
+
+    # creates list that we can sort frequencies
+    for key, value in frequency_of_words.iteritems():
+        frequency_count.append(key)
+
+    frequency_count.sort()
+
+    # printing sorted frequencies on the list of words for that frequency
+    for frequency in frequency_count:
+        for word in frequency_of_words[frequency]:
+            print word, frequency
+
+
+main()
